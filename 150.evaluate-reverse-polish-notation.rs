d87301585228f0829842b25582c93cc07f6739a0
zhiyanzhaijie <zhiyanzhaijie@gmail.com>
2026-05-09 16:43:07 +0800

    feat: 150. Evaluate Reverse Polish Notation

---
+++ 150.evaluate-reverse-polish-notation.rs
@@ -0,0 +1,34 @@
+// @leet start
+impl Solution {
+    pub fn eval_rpn(tokens: Vec<String>) -> i32 {
+        let mut tmp: Vec<i32> = Vec::new();
+
+        for (i, s) in tokens.into_iter().enumerate() {
+            match s.parse::<i32>() {
+                Ok(num) => {
+                    tmp.push(num);
+                }
+                Err(_) => {
+                    if let [.., a, b] = tmp.as_slice() {
+                        let result = match s.as_str() {
+                            "+" => a.checked_add(*b),
+                            "-" => a.checked_sub(*b),
+                            "*" => a.checked_mul(*b),
+                            "/" => a.checked_div(*b),
+                            _ => None,
+                        };
+
+                        if let Some(res) = result {
+                            tmp.truncate(tmp.len() - 2);
+                            tmp.push(res)
+                        }
+                    }
+                }
+            }
+        }
+
+        *tmp.first().unwrap()
+    }
+}
+// @leet end
+
